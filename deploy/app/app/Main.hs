a24b4cf2a64d33e5ab7130738a07427af2a9fc41
nicvaltel <nicvaltel@gmail.com>
2025-05-08 13:37:18 +0300

    Version 1.0 deploy bot

---
+++ deploy/app/app/Main.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NamedFieldPuns #-}
+module Main (main) where
+
+import qualified Data.Text                        as T
+import           Telegram.Bot.API (Token(..))
+import Configuration.Dotenv (parseFile)
+import Data.Either.Combinators(maybeToRight)
+import qualified Bot
+
+newtype BotConfig = BotConfig
+  { botToken :: String
+  }
+
+envFile :: FilePath
+envFile = "app-config.env"
+
+dbFile :: FilePath
+dbFile = "db.env"
+
+main :: IO ()
+main = do
+  cfg <- getCfg <$> parseFile envFile
+  case cfg of
+    Left err -> error err
+    Right BotConfig{botToken} -> do
+      Bot.runBot (Token . T.pack $ botToken) dbFile
+  where
+    getCfg :: [(String, String)] -> Either String BotConfig
+    getCfg env = do
+      botToken <- maybeToRight "No TOKEN defined" (lookup "BOT_TOKEN" env)
+      pure BotConfig {botToken}
